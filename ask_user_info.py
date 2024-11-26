d0391eb87e00c727ef4e624b9c6fb05d836af55b
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-11-26 16:26:28 +0800

    Started with the pseudocode

---
+++ ask_user_info.py
@@ -0,0 +1,17 @@
+# ask user about their:
+    # full name
+    # nickname
+    # age
+    # address
+    # birthday
+    # cellphone number
+    # favorite color
+    # hobby
+    # what would they like to be in the future
+    # favorite food
+    # favorite ice cream flavor
+    # books or songs?
+    # preference on pineapples on pizza? (yes or no)
+    # one word to describe yourself
+    # motto in life
+# ask user again if they want to enter another set of data
