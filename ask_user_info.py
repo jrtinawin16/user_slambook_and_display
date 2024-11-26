3c79c0bfacd93d3511e7e65352b7770f4743eb72
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-11-26 16:53:34 +0800

    Added conditions and prompt to enter nickname and age

--- ask_user_info.py
+++ ask_user_info.py
@@ -20,4 +20,11 @@ while True:
 
     # collect all datas
     full_name = input("Full name: ")
+    if full_name == " ":
+        print ("Invalid, please re-enter your name")
+            
+    # store datas in dictionary
     person_info = {}
+
+    person_info["Nickname"] = input("Nickname: ")
+    person_info["Age"] = int(input("Age: "))
