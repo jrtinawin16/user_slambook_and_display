8d58022c5fd041b1ba8467a32ead843c197728f9
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-11-30 02:23:49 +0800

    Fixed indent and conditions

--- ask_user_info.py
+++ ask_user_info.py
@@ -28,40 +28,45 @@ while True:
                 break
             
         # store datas in dictionary
-        person_info = {}
+            person_info = {}
 
-        person_info["Nickname"] = input("Nickname: ")
-        if person_info["Nickname"] == " ":
-            break
-        person_info["Age"] = (int(input("Age: ")))
-        if person_info["Age"] == " ":
-            break
-        person_info["Address"] = input("Address: ")
-        person_info["Birthday"] = input("Birthday: ")
-        person_info["Cellphone Number"] = (int(input("Cellphone Number: ")))
-        if len(str(person_info["Cellphone Number"])) != 11 or person_info["Cellphone Number"] == " ":
-            break
-        person_info["Favorite Color"] = input("Favorite Color: ")
-        person_info["Hobby"] = input("Hobby: ")
-        person_info["Ambition"] = input("What would you like to be in the future?: ")
-        person_info["Favorite Food"] = input("Favorite food: ")
-        person_info["Favorite Ice Cream Flavor"] = input("Favorite Ice cream flavor: ")
-        person_info["Choice"] = input("Books or songs?: ")
-        if person_info["Choice"] != "Books":
-            print("Invalid, please try again.")
-        elif person_info["Choice"] != "Songs":
-            print("Invalid, please try again.")
-        person_info["Pineapple Preference"] = input("Do you like pineapples on pizza? (y/n): ")
-        if person_info["Pineapple Preference"] != "y" or person_info["Pineapple Preference"] != "n":
-            print("Invalid, please try again.")
-        person_info["Description"] = input("One word to describe yourself: ")
-        person_info["Motto"] = input("Motto in life: ")
+            person_info["Nickname"] = input("Nickname: ")
+            if person_info["Nickname"] == " ":
+                break
+            person_info["Age"] = (int(input("Age: ")))
+            if person_info["Age"] == " ":
+                break
+            person_info["Address"] = input("Address: ")
+            person_info["Birthday"] = input("Birthday: ")
+            person_info["Cellphone Number"] = (int(input("Cellphone Number: ")))
+            if len(str(person_info["Cellphone Number"])) != 11 or person_info["Cellphone Number"] == " ":
+                break
+            person_info["Favorite Color"] = input("Favorite Color: ")
+            person_info["Hobby"] = input("Hobby: ")
+            person_info["Ambition"] = input("What would you like to be in the future?: ")
+            person_info["Favorite Food"] = input("Favorite food: ")
+            person_info["Favorite Ice Cream Flavor"] = input("Favorite Ice cream flavor: ")
+            person_info["Choice"] = input("Books or songs?: ")
+            if person_info["Choice"] != "Books":
+                break
+            elif person_info["Choice"] != "Songs":
+                break
+            person_info["Pineapple Preference"] = input("Do you like pineapples on pizza? (y/n): ")
+            if person_info["Pineapple Preference"] != "y" or person_info["Pineapple Preference"] != "n":
+                break
+            person_info["Description"] = input("One word to describe yourself: ")
+            person_info["Motto"] = input("Motto in life: ")
 
-    except: 
-        print("Invalid input, please try again.")    
+            print("\nInformation successfully saved!")
+            
+            retry = input("Do you want to add another person? (y/n): ")
+            if retry != "y":    
+                continue
+            else:
+                print("Thank you!")
 
-        print("\nInformation successfully saved!")
-    retry = input("Do you want to add another person? (y/n): ")
-    if enter_again != "y":
-        print("Thank you!")
-        break
+        except: 
+            print("Invalid input, please try again.")    
+
+    
+        
