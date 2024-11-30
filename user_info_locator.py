0ea78c5a1db6a7c259ac8f8be3f191bfe36c9dc7
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-11-30 20:22:35 +0800

    Adding more conditions

--- user_info_locator.py
+++ user_info_locator.py
@@ -3,7 +3,17 @@ try:
     with open ("personal_data.txt", "r") as file:
         lines = file.readlines()
         user_info = {}
-        for line in lines
+        for line in lines:
+            if line.strip() == "-------------------------------------":
+                if user_info:
+                    user_info[user_info["full_name"]] = user_info
+                user_info = {}
+                continue
+            line = line.strip()
+            if line.startswith("Full_name: "):
+                user_info['full_name'] = line.split("|")[1].strip()
+            elif line.startswith("Nickname: "):
+                user_info['nickname'] = line.split("|")[1].strip()            
 
 except:
     print("An error occured.")
