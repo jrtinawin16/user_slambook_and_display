52a362c615bcd6f0bde28e7f1d012129780ad5a6
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-11-30 20:43:36 +0800

    Added more conditions

--- user_info_locator.py
+++ user_info_locator.py
@@ -13,7 +13,33 @@ try:
             if line.startswith("Full_name: "):
                 user_info['full_name'] = line.split("|")[1].strip()
             elif line.startswith("Nickname: "):
-                user_info['nickname'] = line.split("|")[1].strip()            
+                user_info['nickname'] = line.split("|")[1].strip()
+            elif line.startswith("Age: "):
+                user_info['user_age'] = line.split("|")[1].strip()
+            elif line.startswith("Address: "):
+                user_info['user_address'] = line.split("|")[1].strip()
+            elif line.startswith("Birthday: "):
+                user_info['user_birthday'] = line.split("|")[1].strip() 
+            elif line.startswith("Cellphone Number: "):
+                user_info['user_number'] = line.split("|")[1].strip()   
+            elif line.startswith("Favorite color: "):
+                user_info['user_color'] = line.split("|")[1].strip() 
+            elif line.startswith("Hobby: "):
+                user_info['user_hobby'] = line.split("|")[1].strip()  
+            elif line.startswith("Ambition: "):
+                user_info['user_ambition'] = line.split("|")[1].strip()   
+            elif line.startswith("Favorite food: "):
+                user_info['user_favorite_food'] = line.split("|")[1].strip() 
+            elif line.startswith("Favorite ice cream flavor: "):
+                user_info["user_ice_cream "] = line.split("|")[1].strip() 
+            elif line.startswith("Books or songs: "):
+                user_info['user_choice'] = line.split("|")[1].strip()    
+            elif line.startswith("Like pineapples on pizza: "):
+                user_info['user_preference'] = line.split("|")[1].strip()   
+            elif line.startswith("One word to describe yourself: "):
+                user_info['user_description '] = line.split("|")[1].strip()
+            elif line.startswith("Motto: "):
+                user_info['user_motto'] = line.split("|")[1].strip()                                
 
 except:
     print("An error occured.")
