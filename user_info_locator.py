ccac4ab125c60c6efef40347821d9057cade37d9
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-12-01 01:48:04 +0800

    Unfortunately, program still has errors

--- user_info_locator.py
+++ user_info_locator.py
@@ -4,45 +4,45 @@ try:
         lines = file.readlines()
         user_data = {}
         for line in lines:
-            if line in lines:
-                if line.strip() == "-------------------------------------":
-                    if user_info:
-                        user_info[user_data["full_name"]] = user_info
-                    user_data = {}
-                    continue
-                line = line.strip()
-                if line.startswith("Full name: "):
-                    user_data['full_name'] = line.split(":", 1)[1].strip()
-                elif line.startswith("Nickname: "):
-                    user_data["nickname"] = line.split(":")[1].strip()
-                elif line.startswith("Age: "):
-                    user_data["user_age"] = line.split(":")[1].strip()
-                elif line.startswith("Address: "):
-                    user_data["user_address"] = line.split(":")[1].strip()
-                elif line.startswith("Birthday: "):
-                    user_data["user_birthday"] = line.split(":")[1].strip()
-                elif line.startswith("Cellphone number: "):
-                    user_data["user_number"] = line.split(":")[1].strip()
-                elif line.startswith("Favorite Color: "):
-                    user_data["user_color"] = line.split(":")[1].strip()
-                elif line.startswith("Hobby: "):
-                    user_data["user_hobby"] = line.split(":")[1].strip() 
-                elif line.startswith("Ambition: "):
-                    user_data["user_ambition"] = line.split(":")[1].strip()   
-                elif line.startswith("Favorite food: "):
-                    user_data["user_favorite_food"] = line.split(":")[1].strip()
-                elif line.startswith("Favorite ice cream flavor: "):
-                    user_data["user_ice_cream"] = line.split(":")[1].strip()
-                elif line.startswith("Books or songs: "):
-                    user_data["user_choice"] = line.split(":")[1].strip()
-                elif line.startswith("Like pineapples on pizza: "):
-                    user_data["user_preference"] = line.split(":")[1].strip()
-                elif line.startswith("One word to describe yourself: "):
-                    user_data["user_description"] = line.split(":")[1].strip()
-                elif line.startswith("Motto: "):
-                    user_data["user_motto"] = line.split(":")[1].strip()
-            if user_data:
-                user_info[user_data['full_name']]
+            line = line.strip()
+            if line == "-------------------------------------":
+                if user_data:
+                        user_info[user_data["full_name"]] = user_data
+                user_data = {}
+                continue
+
+            if line.startswith("Full name: "):
+                user_data['full_name'] = line.split(":", 1)[1].strip()
+            elif line.startswith("Nickname: "):
+                user_data["nickname"] = line.split(":")[1].strip()
+            elif line.startswith("Age: "):
+                user_data["user_age"] = line.split(":")[1].strip()
+            elif line.startswith("Address: "):
+                user_data["user_address"] = line.split(":")[1].strip()
+            elif line.startswith("Birthday: "):
+                user_data["user_birthday"] = line.split(":")[1].strip()
+            elif line.startswith("Cellphone number: "):
+                user_data["user_number"] = line.split(":")[1].strip()
+            elif line.startswith("Favorite Color: "):
+                user_data["user_color"] = line.split(":")[1].strip()
+            elif line.startswith("Hobby: "):
+                user_data["user_hobby"] = line.split(":")[1].strip() 
+            elif line.startswith("Ambition: "):
+                user_data["user_ambition"] = line.split(":")[1].strip()   
+            elif line.startswith("Favorite food: "):
+                user_data["user_favorite_food"] = line.split(":")[1].strip()
+            elif line.startswith("Favorite ice cream flavor: "):
+                user_data["user_ice_cream"] = line.split(":")[1].strip()
+            elif line.startswith("Books or songs: "):
+                user_data["user_choice"] = line.split(":")[1].strip()
+            elif line.startswith("Like pineapples on pizza: "):
+                user_data["user_preference"] = line.split(":")[1].strip()
+            elif line.startswith("One word to describe yourself: "):
+                user_data["user_description"] = line.split(":")[1].strip()
+            elif line.startswith("Motto: "):
+                user_data["user_motto"] = line.split(":")[1].strip()
+        if user_data:
+            user_info[user_data['full_name']] = user_data
         else:   
             print("Person not found.")  
 except:
@@ -51,24 +51,24 @@ if len(user_info) > 0:
     while True:
         try:
             search_name = input("Please enter full name to search: ").strip()
-            if search_name in user_info == 'full_name':
-                user_data = user_info["full_name"]
+            if search_name in user_info:
                 print("\nInformation Found")
-                print(f"Full Name: {user_info['full_name']}")
-                print(f"Nickname: {user_info['nickname']}")
-                print(f"Age: {user_info['user_age']}")
-                print(f"Address: {user_info['user_address']}")
-                print(f"Birthday: {user_info['user_birthday']}")
-                print(f"Cellphone Number: {user_info['user_number']}")
-                print(f"Favorite color: {user_info['user_color']}")
-                print(f"Hobby: {user_info['user_hobby']}")
-                print(f"Ambition: {user_info['user_ambition']}")
-                print(f"Favorite food: {user_info['user_favorite_food']}")
-                print(f"Favorite ice cream flavor: {user_info['user_ice_cream']}")
-                print(f"Books or songs: {user_info['user_choice']}")
-                print(f"Like pineapples on pizza: {user_info['user_preference']}")
-                print(f"One word to describe yourself: {user_info['user_description']}")
-                print(f"Motto: {user_info['user_motto']}")
+                data = user_info[search_name]
+                print(f"Full Name: {data['full_name']}")
+                print(f"Nickname: {data['nickname']}")
+                print(f"Age: {data['user_age']}")
+                print(f"Address: {data['user_address']}")
+                print(f"Birthday: {data['user_birthday']}")
+                print(f"Cellphone Number: {data['user_number']}")
+                print(f"Favorite color: {data['user_color']}")
+                print(f"Hobby: {data['user_hobby']}")
+                print(f"Ambition: {data['user_ambition']}")
+                print(f"Favorite food: {data['user_favorite_food']}")
+                print(f"Favorite ice cream flavor: {data['user_ice_cream']}")
+                print(f"Books or songs: {data['user_choice']}")
+                print(f"Like pineapples on pizza: {data['user_preference']}")
+                print(f"One word to describe yourself: {data['user_description']}")
+                print(f"Motto: {data['user_motto']}")
             else: 
                 print ("Invalid, try again.")
         except:
